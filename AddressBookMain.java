d63e35e76707e5145b7bc10323d49fb10ae83596
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-05 23:55:26 +0530

    Added:code to retrive all the entries from db

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,9 +1,51 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class AddressBookMain {
 
+    public static final int INSERT = 1;
+    public static final int SELECT = 2;
+    private static final int EXIT = 3;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to address book jdbc program");
+        AddressBook addressBook = new AddressBook();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver found");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook", "root", "mokshit@2019");
+            System.out.println("this is connected");
+            Scanner input = new Scanner(System.in);
+            int option = 0;
+            while (option != 3) {
+                System.out.println("Options are : ");
+                System.out.print("1.Insert\t");
+                System.out.print("2.Select\t");
+                System.out.print("3.Exit\n");
+                System.out.print("Enter an option from above :- ");
+                option = input.nextInt();
+
+                switch (option) {
+                    case INSERT:
+                        addressBook.insertRecord(connection);
+                        break;
+                    case SELECT:
+                       addressBook.selectRecord(connection);
+                       break;
+                    case EXIT:
+                        break;
+                }
+            }
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("class is not found please check");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
